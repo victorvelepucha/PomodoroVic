4577f704bbc193d0d558b767c38e535cca3a6306
Victor Velepucha <victor.velepucha@epn.edu.ec>
2025-03-14 12:41:10 -0500

    V1.0 Version inicial, solo con boton de 25 minutos.

---
+++ Pomodoro.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace PomodoroVic
+{
+    partial class Pomodoro
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.btn25Minutos = new System.Windows.Forms.Button();
+            this.timerControlTiempo = new System.Timers.Timer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+            this.lblTiempo.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblTiempo.Location = new System.Drawing.Point(24, 16);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(352, 56);
+            this.lblTiempo.TabIndex = 0;
+            this.lblTiempo.Text = "label1";
+            // 
+            // btn25Minutos
+            // 
+            this.btn25Minutos.Location = new System.Drawing.Point(24, 104);
+            this.btn25Minutos.Name = "btn25Minutos";
+            this.btn25Minutos.Size = new System.Drawing.Size(168, 32);
+            this.btn25Minutos.TabIndex = 1;
+            this.btn25Minutos.Text = "focus 25";
+            this.btn25Minutos.Click += new System.EventHandler(this.btn25Minutos_Click);
+            // 
+            // timerControlTiempo
+            // 
+            this.timerControlTiempo.Interval = 1000;
+            this.timerControlTiempo.SynchronizingObject = this;
+            this.timerControlTiempo.Elapsed += new System.Timers.ElapsedEventHandler(this.timerControlTiempo_Elapsed);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(8, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(384, 213);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn25Minutos);
+            this.Controls.Add(this.lblTiempo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pomodoro v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.DateTime dtmTiempoAuxiliar;
+        private System.DateTime dtmTiempoActualizado;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Button btn25Minutos;
+        private System.Timers.Timer timerControlTiempo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pomodoro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PomodoroVic
+{
+    public partial class Pomodoro : Form
+    {
+        public Pomodoro()
+        {
+            InitializeComponent();
+        }
+        private void btn25Minutos_Click(object sender, System.EventArgs e)
+        {
+            //dtmTiempoAuxiliar = new DateTime(1901, 1, 1, 1, 0, 50);//Tiempo para pruebas en desarrollo
+            dtmTiempoAuxiliar = new DateTime(1901, 1, 1, 1, 0, 0);////dtmTiempoAuxiliar = DateTime.Now; //Para pruebas en desarrollo
+            dtmTiempoActualizado = new DateTime(1901, 1, 1, 1, 0, 0);
+            //dtmTiempoAuxiliar = dtmTiempoAuxiliar.AddMinutes(4);//Temporal para pruebas en desarrollo
+            //dtmTiempoActualizado = dtmTiempoActualizado.AddMinutes(1);//1 min para desarrollo
+            dtmTiempoActualizado = dtmTiempoActualizado.AddMinutes(25);
+            lblTiempo.Text = dtmTiempoActualizado.ToString("mm:ss");
+
+            timerControlTiempo.Start();
+        }
+
+        private void timerControlTiempo_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            dtmTiempoActualizado = dtmTiempoActualizado.Subtract(new TimeSpan(0, 0, 0, 1));
+            lblTiempo.Text = dtmTiempoActualizado.ToString("mm:ss");
+            label1.Text = dtmTiempoAuxiliar.ToString("mm:ss");
+            if (dtmTiempoActualizado <= dtmTiempoAuxiliar)
+            {
+                timerControlTiempo.Stop();
+                MessageBox.Show("Fin del tiempo", "Fin Pomodoro!!!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            //Application.DoEvents();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            lblTiempo.Text = "00:00";
+            label1.Text = lblTiempo.Text;//Temporal
+        }
+    }
+}
